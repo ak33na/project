4eb479ea46f3cc0bfaba5c8740a89a3360e40fb1
ak33na <129073410+ak33na@users.noreply.github.com>
2023-04-18 14:12:50 +0000

    commit

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 var mol = new Kekule.Molecule();
-var atomC = mol.appendAtom('C');
-var atomO1 = mol.appendAtom('O');
-var atomO2 = mol.appendAtom('O');
-mol.appendBond([atomC, atomO1], 2);
-mol.appendBond([atomC, atomO2], 2);
+
+// Create atoms in molecule, adding hydrogens unnecessary
+var atomC1 = mol.appendAtom('C');
+var atomC2 = mol.appendAtom('C');
+var atomC3 = mol.appendAtom('C');
+var atomC4 = mol.appendAtom('C');
+var atomC5 = mol.appendAtom('C');
+
+// Must append bonds between atoms, otherwise registered as unbonded
+mol.appendBond([atomC1, atomC2], 2);
+mol.appendBond([atomC2, atomC3], 1);
+mol.appendBond([atomC3, atomC4], 1);
+mol.appendBond([atomC4, atomC5], 1);
 
 // Get formula
 var formula = mol.calcFormula();
 console.log('Formula: ', formula.getText());
 
-// Output SMILES
+// Output Simplified Molecular Input Line Entry System
 var smiles = Kekule.IO.saveFormatData(mol, 'smi');
 console.log('SMILES: ', smiles);
 
 // Output MOL2k
 var mol2k = Kekule.IO.saveFormatData(mol, 'mol');
 console.log('MOL 2000: \n', mol2k);
+
