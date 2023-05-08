7da2ad30c9d44e638b8b2581eb891c1dfd45b8a9
ak33na <129073410+ak33na@users.noreply.github.com>
2023-05-08 14:23:23 +0000

    Co-authored-by: Jamie-Woodard <Jamie-Woodard@users.noreply.github.com>

--- buildmolecule.js
+++ buildmolecule.js
@@ -2,11 +2,57 @@ var molecule = new Kekule.Molecule();
 let index = 0;
 var C1 =molecule.appendAtom('C');
 
-function createCarbon() {
-    index++;
-    var index = molecule.appendAtom('C');
+let carboni = 1;
+let oxyi = 0;
+let nitroi = 0;
+let haloi = 0;
+let bondi = 0;
+let benzi = 0;
+let ri = 0;
+
+function declareCarbon() {
+    let k = 'C';
+    console.log("Declare carbon works");
+    carboni++;
+    eval('var ' + k + carboni + '= ' + 'C' + ';');
+}
+
+function declareOxygen() {
+    let k = 'O';
+    console.log("Declare oxygen works");
+    oxyi++;
+    eval('var ' + k + oxyi + '= ' + 'O' + ';');
+}
+
+function declareNitrogen() {
+    let k = 'N';
+    console.log("Declare nitrogen works");
+    nitroi++;
+    eval('var ' + k + nitroi + '= ' + 'N' + ';');
+}
+
+function declareHalogen() {
+    let k = 'X';
+    console.log("Declare halogen works");
+    haloi++;
+    eval('var ' + k + haloi + '= ' + 'X' + ';');
 
 }
 
+function declareBond() {
+    let k = 'bond';
+    console.log("Declare bond works");
+    bondi++;
+    eval('var ' + k + bondi + '= ' + 'bond' + ';');
+
+}
+
+function declareBenzene() {
+    console.log("Declare benzene works");
+}
+
+function declareR() {
+    console.log("Declare R works");
+}
 var moleculeFormula = molecule.calcFormula();
 console.log('Formula: ', moleculeFormula.getText());
